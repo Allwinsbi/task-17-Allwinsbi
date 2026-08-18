6b42fd8dad645781526df4312b0cf9b0c03a0917
Allwin <allwinsbi@gmail.com>
2026-08-18 11:02:26 +0530

    first commit

---
+++ src/components/StudentCard.jsx
@@ -0,0 +1,11 @@
+function StudentCard(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>Department: {props.department}</p>
+      <p>CGPA: {props.cgpa}</p>
+    </div>
+  );
+}
+
+export default StudentCard;
